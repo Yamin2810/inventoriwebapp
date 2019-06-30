5885a8d552019941b1c179aeb9826eb8da6ec954
Yamin2810 <51230292+Yamin2810@users.noreply.github.com>
2019-06-30 20:40:20 +0700

    Update daftar.php

--- inventori/daftar.php
+++ inventori/daftar.php
@@ -26,7 +26,7 @@
   <tr><td>Username</td><td> : <input name="username"class="form-control" type="text"></td></tr>
   <tr><td>Password</td><td> : <input name="password" class="form-control" type="password"></td></tr>
   <tr><td colspan="2" align="right"><input value="Daftar" type="submit" class="btn btn-primary"> <input value="Batal" type="reset" class="btn btn-primary"></td></tr>
-  <tr><td colspan="2" align="center">Sudah Punya akun ? <a href="login.php"><b>Login</b></a></td></tr>
+  <tr><td colspan="2" align="center">Sudah Punya akun ? <a href="index.php"><b>Login</b></a></td></tr>
   </tbody>
   </table>
   </form>
