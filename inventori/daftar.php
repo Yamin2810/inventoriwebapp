e762a962d0b9f0410837717ae8a98cf434353755
Yamin2810 <51230292+Yamin2810@users.noreply.github.com>
2019-06-30 20:56:26 +0700

    Update daftar.php

--- inventori/daftar.php
+++ inventori/daftar.php
@@ -2,32 +2,67 @@
    session_start();
    if(isset($_SESSION['username'])) {
    header('location:index.php'); }
+   require_once("database.php");
 ?>
+<link href="assets/addons/bootstrap-4.1.1./css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="assets/addons/bootstrap-4.1.1./js/bootstrap.min.js"></script>
+<script src="assets/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
 <html>
+    
 <head>
-<title>Form Pendaftaran</title>
-<!-- bootstrap-->
-	<link rel="stylesheet" type="text/css" href="assets/addons/bootstrap-3.3.7/css/bootstrap.min.css">
-<!-- css-->
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title>
+      Login Page</title>
+	<link rel="stylesheet" href="assets/addons/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="assets/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="assets/css/all.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/login.css">
 </head>
+
 <body>
-<div class="nav" id="navigasi">
-	<div class="row">
-		<div class="col-md-8" ></div>
-		
+	<div class="container h-100">
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+               <img src="PNJ.png" class="brand_logo" alt="Logo">
+					</div>
+				</div>
+				<div class="d-flex justify-content-center form_container">
+				<form action="prosesdaftar.php" method="post">
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text" name="username" class="form-control input_user">
+						</div>
+						<div class="input-group mb-2">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" name="password" class="form-control input_pass">
+						</div>
+						<div class="form-group">
+							
+						</div>
+					
+				</div>
+				<div class="d-flex justify-content-center mt-3 login_container">
+					<button type="submit" name="button" class="btn login_btn">Sign Up</button>
+        </div>
+        <div class="mt-4">
+					<div class="d-flex justify-content-center links">
+						have an account? <a href="index.php" class="ml-2">Login</a>
+					</div>
+        
+				
+					
+   </form>
+				</div>
+			</div>
+		</div>
 	</div>
-</div>
-<div align='center'>
-  <form action="prosesdaftar.php" method="post">
-  <table>
-  <tbody>
-  <tr><td colspan="2" align="center"><h1>Daftar Baru</h1></td></tr>
-  <tr><td>Username</td><td> : <input name="username"class="form-control" type="text"></td></tr>
-  <tr><td>Password</td><td> : <input name="password" class="form-control" type="password"></td></tr>
-  <tr><td colspan="2" align="right"><input value="Daftar" type="submit" class="btn btn-primary"> <input value="Batal" type="reset" class="btn btn-primary"></td></tr>
-  <tr><td colspan="2" align="center">Sudah Punya akun ? <a href="index.php"><b>Login</b></a></td></tr>
-  </tbody>
-  </table>
-  </form>
-</div>
+</body>
+</html>
