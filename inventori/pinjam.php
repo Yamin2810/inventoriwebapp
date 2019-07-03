c9e698326b6f657a1475e62027ddbafbeeaddc93
Yamin2810 <51230292+Yamin2810@users.noreply.github.com>
2019-07-03 07:04:16 +0700

    Add files via upload

--- inventori/pinjam.php
+++ inventori/pinjam.php
@@ -9,10 +9,12 @@ $username= $_POST['username'];
 
 
 
-$query ="insert into pinjam values ('','$username', '$barang')";
+$query ="insert into pinjam values ('','$username', '$barang',NOW())";
+$query ="insert into history values ('','$username', '$barang',NOW())";
 $result = mysqli_query($connection, $query);
-
-if ($result) {
+//if ($result) {
+	if($connection->multi_query($query)== TRUE){
+	echo "New records created successfully";
 	header("location: halaman_pengguna.php");
 } else {
 	printf(mysqli_error($connection));
